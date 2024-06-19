1187241e6a1f345738f0d39ec225cce2c4d0f918
sofiamazzurco <mazzurquito582@gmail.com>
2024-06-19 20:36:28 -0300

    UserRepository, Iuser

---
+++ TPI_Prod_Veterinaria/Domain/Entities/Diagnostico.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Diagnostico
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(180)]
+        public string Descripcion { get; set; }
+
+        [Column(TypeName = "datetime")]
+        public DateTime CreationDate { get; private set; } = DateTime.Now;
+        //fehca de modificación??
+    }
+}
---
+++ TPI_Prod_Veterinaria/Domain/Entities/Mascota.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Mascota
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        [ForeignKey("DiagnosticoId")]
+        public Diagnostico diagnosticoSegunMasc { get; set; }
+
+        public bool Estado { get; set; } //si lo retiró o no
+    }
+}
